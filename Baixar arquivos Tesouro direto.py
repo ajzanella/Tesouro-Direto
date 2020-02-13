ff8324cc3dfaf3f6151f78a7799bf0282bd38c12
Angelo Zanella <ajzanella@gmail.com>
2020-02-13 05:11:47 -0300

    rodar no py ano anterior e atual

--- Baixar arquivos Tesouro direto.py
+++ Baixar arquivos Tesouro direto.py
@@ -32,49 +32,6 @@ path = a['href']
 name = a.string
 download_url = base_url + path
 ssl._create_default_https_context = ssl._create_unverified_context
-urllib.request.urlretrieve(download_url, "C:/Users/ajzan/Documents/GitHub/Tesouro-Direto/DataBase/Historic/LFT/{}_%s.xls".format(name) %(x-1))
-
-a = div.find_all('a')[1]
-path = a['href']
-name = a.string
-download_url = base_url + path
-ssl._create_default_https_context = ssl._create_unverified_context
-urllib.request.urlretrieve(download_url, "C:/Users/ajzan/Documents/GitHub/Tesouro-Direto/DataBase/Historic/LTN/{}_%s.xls".format(name) %(x-1))
-
-a = div.find_all('a')[2]
-path = a['href']
-name = a.string
-download_url = base_url + path
-ssl._create_default_https_context = ssl._create_unverified_context
-urllib.request.urlretrieve(download_url, "C:/Users/ajzan/Documents/GitHub/Tesouro-Direto/DataBase/Historic/NTN-C/{}_%s.xls".format(name) %(x-1))
-
-a = div.find_all('a')[3]
-path = a['href']
-name = a.string
-download_url = base_url + path
-ssl._create_default_https_context = ssl._create_unverified_context
-urllib.request.urlretrieve(download_url, "C:/Users/ajzan/Documents/GitHub/Tesouro-Direto/DataBase/Historic/NTN-B/{}_%s.xls".format(name) %(x-1))
-
-a = div.find_all('a')[4]
-path = a['href']
-name = a.string
-download_url = base_url + path
-ssl._create_default_https_context = ssl._create_unverified_context
-urllib.request.urlretrieve(download_url, "C:/Users/ajzan/Documents/GitHub/Tesouro-Direto/DataBase/Historic/NTN-BPrincipal/{}_%s.xls".format(name) %(x-1))
-
-a = div.find_all('a')[5]
-path = a['href']
-name = a.string
-download_url = base_url + path
-ssl._create_default_https_context = ssl._create_unverified_context
-urllib.request.urlretrieve(download_url, "C:/Users/ajzan/Documents/GitHub/Tesouro-Direto/DataBase/Historic/NTN-F/{}_%s.xls".format(name) %(x-1))
-
-####baixar ano atual
-a = div.find_all('a')[0]
-path = a['href']
-name = a.string
-download_url = base_url + path
-ssl._create_default_https_context = ssl._create_unverified_context
 urllib.request.urlretrieve(download_url, "C:/Users/ajzan/Documents/GitHub/Tesouro-Direto/DataBase/Historic/LFT/{}_%s.xls".format(name) %(x))
 
 a = div.find_all('a')[1]
@@ -111,5 +68,54 @@ name = a.string
 download_url = base_url + path
 ssl._create_default_https_context = ssl._create_unverified_context
 urllib.request.urlretrieve(download_url, "C:/Users/ajzan/Documents/GitHub/Tesouro-Direto/DataBase/Historic/NTN-F/{}_%s.xls".format(name) %(x))
+
+
+
+
+
+
+
+####baixar ano atual
+a = div.find_all('a')[6]
+path = a['href']
+name = a.string
+download_url = base_url + path
+ssl._create_default_https_context = ssl._create_unverified_context
+urllib.request.urlretrieve(download_url, "C:/Users/ajzan/Documents/GitHub/Tesouro-Direto/DataBase/Historic/LFT/{}_%s.xls".format(name) %(x-1))
+
+a = div.find_all('a')[7]
+path = a['href']
+name = a.string
+download_url = base_url + path
+ssl._create_default_https_context = ssl._create_unverified_context
+urllib.request.urlretrieve(download_url, "C:/Users/ajzan/Documents/GitHub/Tesouro-Direto/DataBase/Historic/LTN/{}_%s.xls".format(name) %(x-1))
+
+a = div.find_all('a')[8]
+path = a['href']
+name = a.string
+download_url = base_url + path
+ssl._create_default_https_context = ssl._create_unverified_context
+urllib.request.urlretrieve(download_url, "C:/Users/ajzan/Documents/GitHub/Tesouro-Direto/DataBase/Historic/NTN-C/{}_%s.xls".format(name) %(x-1))
+
+a = div.find_all('a')[9]
+path = a['href']
+name = a.string
+download_url = base_url + path
+ssl._create_default_https_context = ssl._create_unverified_context
+urllib.request.urlretrieve(download_url, "C:/Users/ajzan/Documents/GitHub/Tesouro-Direto/DataBase/Historic/NTN-B/{}_%s.xls".format(name) %(x-1))
+
+a = div.find_all('a')[10]
+path = a['href']
+name = a.string
+download_url = base_url + path
+ssl._create_default_https_context = ssl._create_unverified_context
+urllib.request.urlretrieve(download_url, "C:/Users/ajzan/Documents/GitHub/Tesouro-Direto/DataBase/Historic/NTN-BPrincipal/{}_%s.xls".format(name) %(x-1))
+
+a = div.find_all('a')[11]
+path = a['href']
+name = a.string
+download_url = base_url + path
+ssl._create_default_https_context = ssl._create_unverified_context
+urllib.request.urlretrieve(download_url, "C:/Users/ajzan/Documents/GitHub/Tesouro-Direto/DataBase/Historic/NTN-F/{}_%s.xls".format(name) %(x-1))
 
 
