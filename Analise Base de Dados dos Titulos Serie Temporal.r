b1e688b6d6e70cc57043e77c8ad5868c9337b52d
Angelo Zanella <ajzanella@gmail.com>
2020-08-09 09:09:07 -0300

    add colunm

--- Analise Base de Dados dos Titulos Serie Temporal.r
+++ Analise Base de Dados dos Titulos Serie Temporal.r
@@ -3,6 +3,7 @@ library(caTools)
 library(tidyverse)
 library(dslabs)
 library(ggplot2)
+library(stringr)
 
 options(digits = 6) 
 options(scipen=999)
@@ -18,3 +19,6 @@ head(DataBase)
 
 TitulosNames<-unique(DataBase[,1])
 length(TitulosNames)
+DataBase <- mutate(DataBase, ClassTitulos = substring(Titulos.e.Vencimento, 1,str_locate(pattern=" ", Titulos.e.Vencimento)[,1]-1))
+DataBase <- mutate(DataBase, Vencimento = substring(Titulos.e.Vencimento, str_locate(pattern=" ", Titulos.e.Vencimento)[,1]+1,nchar(as.character(Titulos.e.Vencimento))))
+#comverter to Date
--- Analise dos dados Modelos Studio - Titulos.R
+++ Analise dos dados Modelos Studio - Titulos.R
@@ -1,13 +1,23 @@
 #Functions deep learning (Statistics Model)
 library(caTools)
+library(tidyverse)
+library(dslabs)
+library(ggplot2)
 
+options(digits = 6) 
+options(scipen=999)
 
 FileAddress<-"C:/Users/ajzan/Documents/GitHub/Tesouro-Direto/DataBase/"
 setwd(FileAddress)
 
-DataBase<-read.table(paste(getwd(),"CompletedDataBase.txt", sep="/"),header = TRUE, sep="\t", dec=".")
+DataBase<-read.table(paste(getwd(),"CompletedDataBase.txt", sep="/"),header = TRUE, sep="\t", dec=",")
 #convert factor to date
 DataBase[,2]<-as.Date(DataBase[,2])
+
+head(DataBase)
+
+TitulosNames<-unique(DataBase[,1])
+length(TitulosNames)
 
 #filter if have any missing data
 if(nrow(DataBase[is.na(DataBase),])>0){
